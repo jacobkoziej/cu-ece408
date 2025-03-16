e60b3c31484912837c7ecb92e48a8e89d2507af0
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-16 04:16:25 -0400

    Define `decode_rate()`

---
+++ projects/midterm/plcp.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# plcp.py -- OFDM PLCP sublayer
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+from typing import Final
+
+_DECODE_RATE: Final[dict[int, int]] = {
+    0b1011: 6,
+    0b1111: 9,
+    0b1010: 12,
+    0b1110: 18,
+    0b1001: 24,
+    0b1101: 36,
+    0b1000: 48,
+    0b1100: 54,
+}
+
+
+def decode_rate(rate: int) -> int:
+    try:
+        return _DECODE_RATE[rate]
+
+    except Exception as _:
+        raise KeyError(f"Unsupported rate: {bin(rate)}")
