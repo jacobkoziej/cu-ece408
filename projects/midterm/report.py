55e975fb033d993a7c3a4e5b94524094b81626d8
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-27 01:16:31 -0400

    Add `report.py`

---
+++ projects/midterm/report.py
@@ -0,0 +1,13 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# report.py -- IEEE Std 802.11a-1999
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
