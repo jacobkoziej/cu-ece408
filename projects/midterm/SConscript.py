a741d8f24375cc0ba6a5f64a5e394aba0ecc5669
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-27 03:24:57 -0400

    Add `*.py` as a dependency

--- projects/midterm/SConscript.py
+++ projects/midterm/SConscript.py
@@ -13,10 +13,11 @@ from SCons.Script import (
 
 Import("env")
 
-notebook_raw = env.Jupytext("report-raw.ipynb", "report.py")[0]
+report_raw = env.Jupytext("report-raw.ipynb", "report.py")[0]
 
-notebook = env.Papermill("report.ipynb", notebook_raw)[0]
+report = env.Papermill("report.ipynb", report_raw)[0]
+env.Depends(report, env.Glob("*.py"))
 
-report = env.NbConvert(str(Path(str(notebook)).with_suffix(".html")), notebook)
+report = env.NbConvert(str(Path(str(report)).with_suffix(".html")), report)
 
 Return("report")
