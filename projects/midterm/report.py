7e78321eeed7d4016a1fd1da7ac07a839c3ed835
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-27 02:06:28 -0400

    Add `PPDU Frame Format`

--- projects/midterm/report.py
+++ projects/midterm/report.py
@@ -19,3 +19,22 @@
 # I've taken the liberty of writing a Viterbi hard-decision decoder for
 # the receiver along with various unit tests to ensure that each of the
 # sub-components of the TX/RX pair behave as expected.
+
+# %% [markdown]
+# # PPDU Frame Format
+#
+# ```
+# |<------------------- PLCP Header ------------------>|
+# +------+----------+--------+--------+------+---------+------+------+-----+
+# | RATE | RESERVED | LENGTH | PARITY | TAIL | SERVICE | PSDU | TAIL | PAD |
+# | 4b   | 1b       | 12b    | 1b     | 6b   | 16b     |      | 6b   |     |
+# +------+----------+--------+--------+------+---------+------+------+-----+
+# |                                          |                             |
+# +------------------------+ Coded / OFDM    | Coded / OFDM                |
+#                          | (BPSK, r=1/2)   | (RATE indicated in SIGNAL)  |
+#                          |<--------------->|<--------------------------->|
+#          +---------------+-----------------+-----------------------------+
+#          | PLCP Preamble | SIGNAL          | DATA                        |
+#          | 12 symbols    | One OFDM Symbol | Variable OFDM Symbols       |
+#          +---------------+-----------------+-----------------------------+
+# ```
