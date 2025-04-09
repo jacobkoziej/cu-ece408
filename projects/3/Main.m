45ab2d39b728f9108368bfdcd110d8c2cd0141b8
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-08 20:45:27 -0400

    Invert the PN sequence

--- projects/3/Main.m
+++ projects/3/Main.m
@@ -37,8 +37,23 @@ B_RCOS = [
           +0.0052
           +0.0038
          ]';
+WALSH_CHANNELS = 8;
+FRAME_CHIPS = 255;
+PN_TAPS = [8, 7, 6, 1, 0];
 
 %% Apply Root Raised Cosine Filter at Receiver
 load Rcvd_Koziej.mat;
 
-r = upfirdn(Rcvd, B_RCOS, 1, OVERSAMPLE);
+r = downsample(filter(B_RCOS, 1, Rcvd), OVERSAMPLE);
+r = reshape(r, FRAME_CHIPS, []);
+
+%% Invert PN Sequence
+pn_seqs = pskmod(pn_sequences(PN_TAPS), 2);
+
+pilot = r(:, 1);
+pn_correlations = pn_seqs .* pilot;
+[~, initial_condition] = max(abs(sum(pn_correlations)));
+
+pn_sequence = pn_seqs(:, initial_condition);
+
+r = r .* pn_sequence;
