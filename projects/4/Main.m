43bec6f1188973fda1d99e9c9f836457f8ec22fc
Jacob Koziej <jacobkoziej@gmail.com>
2025-05-11 15:18:01 -0400

    Simulate 2x2 flat fading MIMO channel

--- projects/4/Main.m
+++ projects/4/Main.m
@@ -6,3 +6,41 @@
 clc;
 clear;
 close all;
+
+rng(0x93970dda);
+
+% simulation parameters
+TRAINING_SYMBOLS = 128;
+MESSAGE_SYMBOLS  = 1024;
+BITS_PER_SYMBOL  = 4;
+MOD_FUNC         = @qammod;
+DEMOD_FUNC       = @qamdemod;
+TX_CHANNELS      = 2;
+RX_CHANNELS      = 2;
+FADING_VARIANCE  = 1;
+SNR              = 40;
+
+%%% Simulation Data
+M = 2^BITS_PER_SYMBOL;
+
+training_bits = randi(M - 1, TX_CHANNELS, TRAINING_SYMBOLS);
+message_bits  = randi(M - 1, TX_CHANNELS, MESSAGE_SYMBOLS);
+
+X_train   = MOD_FUNC(training_bits, M);
+X_message = MOD_FUNC(message_bits, M);
+
+%%% Flat Fading Channel
+CN = @(variance, M, N) sqrt(variance / 2) .* (randn(M, N) + 1j * randn(M, N));
+
+H = CN(FADING_VARIANCE, RX_CHANNELS, TX_CHANNELS);
+
+%%% Additive White Gaussian Noise
+P_signal = mean(abs([X_train, X_message]), 'all');
+P_noise  = P_signal / (10^(SNR / 10));
+
+N_train   = CN(P_noise, RX_CHANNELS, TRAINING_SYMBOLS);
+N_message = CN(P_noise, RX_CHANNELS, MESSAGE_SYMBOLS);
+
+%%% Received Data
+Y_train   = (H * X_train) + N_train;
+Y_message = (H * X_message) + N_message;
